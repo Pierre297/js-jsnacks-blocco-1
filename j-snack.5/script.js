0419a4765bdfe56cebb43c8191813ef7360d7c9c
Pier <pierfrancesco297@gmail.com>
2021-10-29 11:40:57 +0200

    j.snack 6

--- j-snack.5/script.js
+++ j-snack.5/script.js
@@ -5,5 +5,15 @@
 // array vuoto
 const numberArr = [];
 
-const numberOne = parseInt(prompt("scegli un numero"));
+// chiedi per 6 volte un numero
+for(let i = 0; i < 6; i++) {
+    let num = parseInt(prompt("Scrivi un numero."));
+
+// definisci se il numero è dispari o meno (se si inseriscilo nell'Array)
+    if(num % 2 !== 0){
+        numberArr.push(num);
+    }
+}
+
+console.log(numberArr);
 
--- j-snack.6/script.js
+++ j-snack.6/script.js
@@ -2,25 +2,44 @@
 
 
 // crea un array e dichiara lo spazio all'interno di esso.
-const spaceArray = 50;
+// const spaceArray = 50;
 
-const randomArray = [];
+// const randomArray = [];
 
-let found = false;
+// let found = false;
 
-let numbers = Math.floor(Math.random() * 1000) +1;
+// let numbers = Math.floor(Math.random() * 1000) +1;
 
-// fai scegliere numeri random in un ciclo for
-for(let i = 0; i < spaceArray; i++ ){
-    // fai in modo che non vengano estratti doppioni
-    if (numbers[i] == spaceArray) {
-        found = true;
+// // fai scegliere numeri random in un ciclo for
+// for(let i = 0; i < spaceArray; i++ ){
+//     // fai in modo che non vengano estratti doppioni
+//     if (numbers[i] == spaceArray) {
+//         found = true;
+//     }
+// };
+
+// if (!found)  {
+//  let numList = document.createElement('div');
+//  numList.innerText = numbers;
+//  document.querySelector('.numbers').append(numList);
+// }
+
+const numberList = [];
+
+while (numberList.length < 50) {
+    const num = Math.floor(Math.random() * 100) + 1;
+
+    let duplicateNum = false;
+
+    for (i = 0; i < numberList.length; i ++) {
+        if(num === numberList[i]){
+            duplicateNum = true;
+        }
     }
-};
-
-if (!found)  {
- let numList = document.createElement('div');
- numList.innerText = numbers;
- document.querySelector('.numbers').append(numList);
+    if (duplicateNum == false){
+        numberList.push(num);
+    }
 }
 
+console.log(numberList);
+console.log("la lista di numeri casuali senza duplicati è: " + numberList);
